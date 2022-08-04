f4c07969e93b0c45493f3fd9e1a3770a21102637
KrazyManJ <KrazyManJ@gmail.com>
2022-08-04 12:29:25 +0200

    Updated style to make it look like Islands GUI

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import pyautogui
+import pyglet
 from idlelib.tooltip import Hovertip
 from threading import Thread, Event
 from tkinter import Tk, Label, LabelFrame, Entry, Button, Variable, Checkbutton
@@ -189,6 +190,7 @@ class FishBotThread(Thread):
             pyautogui.sleep(0.1)
 
 
+
 class GUI:
 
     Vars: dict[str, Variable] = {}
@@ -199,23 +201,46 @@ class GUI:
     @staticmethod
     def drawAndOpen():
         win = GUI.win
+        win.configure(background="#FEFEFD")
         win.title("FishBot")
-        win.geometry("400x600")
+        win.geometry("400x730")
         #win.geometry(f"425x{pyautogui.size().height - 75}+{pyautogui.size().width - 425}+0")
         win.iconbitmap(GUI.__resourcePath("icon.ico"))
-        win.minsize(400, 640)
+        win.minsize(400, 730)
 
-        Label(win, text="FishBot", font=("Segoe UI", 50)).pack()
-        Label(win, text="Made by Kr4zyM4nJ", font=("Segoe UI", 10)).pack(pady=(0, 10))
+        titleFont = "HirukoPro-Black"
+        textFont = "Magdelin Text"
+        pyglet.font.add_file(f"{titleFont}.otf")
+        pyglet.font.add_file("Magdelin Text.otf")
 
-        settingsFrame = LabelFrame(win, text="Settings (Hover for description)", labelanchor="n")
+        Label(win,text="FishBot",bg="#FEFEFD",font=(titleFont, 50)).pack()
+        Label(win,text="Made by Kr4zyM4nJ",bg="#FEFEFD",font=(textFont, 15)).pack(pady=(0, 10))
+
+        settingsFrame = LabelFrame(win,
+                                   text="Settings (Hover for description)",
+                                   font=(titleFont,15),
+                                   labelanchor="n",
+                                   background="#EFE6D3",
+                                   borderwidth=0
+                                   )
         for i in range(3): settingsFrame.grid_columnconfigure(i, weight=1)
-        settingsFrame.pack(fill="x", padx=10, pady=(0, 10), ipady=5)
+        settingsFrame.pack(fill="x", padx=10, pady=(0, 10), ipady=10)
         GUI.Elems["settings_frame"] = settingsFrame
 
-        rodKeyLabel = Label(settingsFrame, text="Fishing rod slot keybind: ")
+        rodKeyLabel = Label(settingsFrame,
+                            text="Fishing rod slot keybind: ",
+                            font=(textFont, 13),
+                            bg="#EFE6D3"
+                            )
         rodKeyLabel.grid(row=0, column=0, columnspan=2)
-        Entry(settingsFrame, width=10, justify="center", textvariable=GUI.__regVar("settings_rod_key", "1", GUI.__charBinding)).grid(row=0, column=2)
+        Entry(settingsFrame,
+              width=5,
+              justify="center",
+              textvariable=GUI.__regVar("settings_rod_key", "1", GUI.__charBinding),
+              background="#C8BAA3",
+              font=(textFont, 13),
+              borderwidth=0
+              ).grid(row=0, column=2)
         Hovertip(rodKeyLabel, """
 When fish didn't appear for while (usually it is because of lag and
 character somehow didn't cast a fishing rod), character will automatically
@@ -225,12 +250,31 @@ try to re-switch tool.
 
 
         for data in [("1","2",2),("2","3",3)]:
-            lab = Label(settingsFrame, text=f"Tier {data[0]} bait:")
+            lab = Label(settingsFrame,
+                        text=f"Tier {data[0]} bait:",
+                        font=(textFont, 13),
+                        bg="#EFE6D3"
+                        )
             lab.grid(row=data[2], column=0)
-            Checkbutton(settingsFrame, variable=GUI.__regVar(f"settings_use_bait{data[0]}", False, GUI.__baitCheckBox)).grid(
+            Checkbutton(settingsFrame,
+                        variable=GUI.__regVar(f"settings_use_bait{data[0]}", False, GUI.__baitCheckBox),
+                        bg="#EFE6D3",
+                        selectcolor="#C8BAA3",
+                        activebackground="#EFE6D3",
+
+                        ).grid(
                 row=data[2], column=1)
-            GUI.Elems[f"settings_bait{data[0]}_key"] = Entry(settingsFrame, width=10, justify="center", state="disabled",
-                                                    textvariable=GUI.__regVar(f"settings_bait{data[0]}_key", data[1], GUI.__charBinding))
+            GUI.Elems[f"settings_bait{data[0]}_key"] = Entry(settingsFrame,
+                                                             width=5,
+                                                             justify="center",
+                                                             state="disabled",
+                                                             textvariable=GUI.__regVar(f"settings_bait{data[0]}_key", data[1], GUI.__charBinding),
+                                                             background="#C8BAA3",
+                                                             disabledbackground="#C8BAA3",
+                                                             font=(textFont, 13),
+                                                             borderwidth=0,
+
+                                                             )
             GUI.Elems[f"settings_bait{data[0]}_key"].grid(row=data[2], column=2)
             Hovertip(lab,f"""
 If enabled, character will always try to use Tier {data[0]} bait before casting fishing rod in
@@ -239,31 +283,53 @@ interval of 2 minutes (this is how long Tier {data[0]} bait lasts).
 
 
 
-        GUI.Elems["start_button"] = Button(win, text="Start", font=("Segoe UI", 20), bg="#00ff00", command=GUI.__btnClick)
-        GUI.Elems["start_button"].pack(fill="x", padx=10, pady=(0, 5))
+        GUI.Elems["start_button"] = Button(win,
+                                           font=(titleFont, 25),
+                                           fg="white",
+                                           command=GUI.__btnClick,
+                                           borderwidth=0,
+                                           )
+        GUI.Elems["start_button"].pack(fill="x", padx=10, pady=(0, 10))
+        GUI.toggleButton(True)
 
-        statusFrame = LabelFrame(win, text="Status", labelanchor="n")
-        statusFrame.pack(fill="x", padx=10, ipady=5)
-        Label(statusFrame, textvariable=GUI.__regVar("status", "Ready to use! Hit \"Start\" to begin!"), font=("Segoe UI", 10)).pack()
+        statusFrame = LabelFrame(win,
+                                 background="#EFE6D3",
+                                 text="Status",
+                                 labelanchor="n",
+                                 borderwidth=0,
+                                 font=(titleFont, 15),
+                                 )
+        statusFrame.pack(fill="x", padx=10, ipady=5, pady=(0, 10))
+        Label(statusFrame,
+              background="#EFE6D3",
+              textvariable=GUI.__regVar("status", "Ready to use! Hit \"Start\" to begin!"),
+              font=(textFont, 12),
+              ).pack()
 
-        statsFrame = LabelFrame(win, text="Statistics", labelanchor="n")
+        statsFrame = LabelFrame(win,
+                                background="#F5F5F4",
+                                text="Statistics",
+                                labelanchor="n",
+                                font=(titleFont, 15),
+                                borderwidth=0
+                                )
         statsFrame.pack(fill="x", padx=10, ipady=5)
         for i in range(4): statsFrame.grid_columnconfigure(i, weight=1)
 
-        Label(statsFrame, text="Time Elapsed:").grid(row=0, column=0)
-        Label(statsFrame, textvariable=GUI.__regVar("time_elapsed", "00h 00m 00s")).grid(row=0, column=1)
-        Label(statsFrame, text="Catch amount:").grid(row=0, column=2)
-        Label(statsFrame, textvariable=GUI.__regVar("total_catch_amount", 0)).grid(row=0, column=3)
+        Label(statsFrame, background="#F5F5F4", font=(textFont, 11), text="Time Elapsed:").grid(row=0, column=0)
+        Label(statsFrame, background="#F5F5F4", font=(textFont, 11), textvariable=GUI.__regVar("time_elapsed", "00h 00m 00s")).grid(row=0, column=1)
+        Label(statsFrame, background="#F5F5F4", font=(textFont, 11), text="Catch amount:").grid(row=0, column=2)
+        Label(statsFrame, background="#F5F5F4", font=(textFont, 11), textvariable=GUI.__regVar("total_catch_amount", 0)).grid(row=0, column=3)
 
         for caType in ["fish", "treasure"]:
-            tempLF = LabelFrame(statsFrame, text=caType.title())
-            tempLF.grid(columnspan=4, sticky="NESW", padx=10, ipady=5)
+            tempLF = LabelFrame(statsFrame, text=caType.title(),background="#EFE6D3",labelanchor="n",borderwidth=0,font=(titleFont, 13))
+            tempLF.grid(columnspan=4, sticky="NESW", padx=10, ipady=5, pady=(10,0))
             for i in range(2): tempLF.grid_columnconfigure(i, weight=1)
             for i in range(0, RARITY_COLORS.keys().__len__()):
                 caRar = list(RARITY_COLORS.keys()).__getitem__(i)
                 if f"{caRar}_{caType}" not in LOOT_TYPE_BLACKLIST:
-                    Label(tempLF, text=caRar.title()).grid(row=i, column=0)
-                    Label(tempLF, textvariable=GUI.__regVar(f"{caRar}_{caType}", 0)).grid(row=i, column=1)
+                    Label(tempLF, text=caRar.title(),background="#EFE6D3",font=(textFont, 11)).grid(row=i, column=0)
+                    Label(tempLF, textvariable=GUI.__regVar(f"{caRar}_{caType}", 0),background="#EFE6D3",font=(textFont, 11)).grid(row=i, column=1)
         win.protocol("WM_DELETE_WINDOW", lambda: win.destroy())
         Listener(on_press=GUI.__keyboardHandle).start()
         win.mainloop()
@@ -271,7 +337,7 @@ interval of 2 minutes (this is how long Tier {data[0]} bait lasts).
     @staticmethod
     def toggleButton(state: bool) -> None:
         GUI.Elems["start_button"].__setitem__("text","Start" if state else "Stop (End)")
-        GUI.Elems["start_button"].__setitem__("bg","#00ff00" if state else "#ff0000")
+        GUI.Elems["start_button"].__setitem__("bg","#0BB41C" if state else "#DC3545")
         for child in GUI.Elems["settings_frame"].winfo_children():
             child["state"] = "normal" if state else "disabled"
         if state:
